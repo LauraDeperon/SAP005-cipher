853f1f9b5ce86ea3b55070ca16a703706b1a6814
Laura Deperon <lauradeperonribeiro@gmail.com>
2020-10-30 22:22:52 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -1,87 +1,40 @@
+const tamAlfabeto=26;
+let cod1letra=65;
 const cipher = {
-  // ...
-};
-
+  encode (offsetCod,msgParaCod){
+    let resultMsgCod="";
+    if (typeof offsetCod == "number" && typeof msgParaCod == "string"){
+        for (let i = 0; i < msgParaCod.length; i++) {
+          const codASC = msgParaCod.charCodeAt(i);
+          let MsgCod = ""     
+          const desloc = ((codASC-cod1letra+offsetCod)%tamAlfabeto)+cod1letra;
+          MsgCod=String.fromCharCode(desloc);
+          resultMsgCod += MsgCod;
+        }
+    }else {
+      throw new TypeError();
+    }
+    return resultMsgCod;
+  },
+  
+  decode (offsetDecod,msgParaDecod){
+    let resultMsgDecod="";
+    if (typeof offsetDecod == "number" && typeof msgParaDecod == "string"){
+      for (let i = 0; i < msgParaDecod.length; i++) {
+        const codASC = msgParaDecod.charCodeAt(i);
+        let MsgDecod = "";
+        const desloc = ((codASC+cod1letra-offsetDecod)%tamAlfabeto)+cod1letra;
+        MsgDecod=String.fromCharCode(desloc);
+        resultMsgDecod += MsgDecod;
+      }
+    }else {
+      throw new TypeError();
+    }  
+    return resultMsgDecod;
+  }
+}
 export default cipher;
 
-const tamAlfabeto=26;
-let resultMsgCod="";
-const btnCodificar = document.getElementById("codificar");
-btnCodificar.addEventListener("click", encode);
-let resultMsgDecod="";
-const btnDecodificar = document.getElementById("decodificar");
-btnDecodificar.addEventListener("click", decode);
-
-function encode(eventoDeClique){
-  eventoDeClique.preventDefault();
-  
-  let msgCod = document.getElementById("msgCod").value;
-  let offsetCod = Number(document.getElementById("offsetCod").value);
-  
-  for (let i = 0; i < msgCod.length; i++) {
-    const codASC = msgCod.charCodeAt(i);
-    let MsgCod = "";
-
-    if (codASC >= 65 && codASC <= 90) {
-      let cod1letra=65;
-      const desloc = ((codASC-cod1letra+offsetCod)%tamAlfabeto)+cod1letra;
-      console.log(desloc);
-      MsgCod=String.fromCharCode(desloc);
-      console.log(MsgCod);
-    } else if (codASC >= 97 && codASC <= 122) {
-      let cod1letra=97;
-      const desloc = ((codASC-cod1letra+offsetCod)%tamAlfabeto)+cod1letra;
-      console.log(desloc);
-      MsgCod=String.fromCharCode(desloc);
-      console.log(MsgCod);
-    } else if (codASC === 32) {
-      MsgCod=String.fromCharCode(" ");
-      console.log(MsgCod);
-    } else if (codASC === 44) {
-      MsgCod=String.fromCharCode(",");
-      console.log(MsgCod);
-    } else if (codASC === 46) {
-      MsgCod=String.fromCharCode(".");
-      console.log(MsgCod);
-    } else{
-     alert("Digite apenas os caracteres aceitos!");
-     window.location.reload();
-     break;
-    }
-    resultMsgCod += MsgCod;
-  }
-  document.getElementById("resultCod").innerHTML=resultMsgCod;
-  return resultMsgCod;
-}
 
 
-function decode(eventoDeClique){
-  eventoDeClique.preventDefault()
- 
-  let msgDecod = document.getElementById("msgDecod").value;
-  let offsetCod = Number(document.getElementById("offsetDecod").value);
-  
-  for (let i = 0; i < msgDecod.length; i++) {
-    const codASC = msgDecod.charCodeAt(i);
-    
-    if (codASC >= 65 && codASC <= 90) {
-      const desloc = ((codASC-cod1letra-offsetCod)%tamAlfabeto)+cod1letra;
-      console.log(desloc);
-      let MsgDecod=String.fromCharCode(desloc);
-      console.log(MsgDecod);
-      resultMsgDecod += msgDecod;
-    }
-    else if (codASC === 32) {
-      let MsgDecod=String.fromCharCode(" ");
-      console.log(MsgDecod);
-      resultMsgDecod += msgDecod;
-    }
-    else{
-     alert("Digite apenas os caracteres aceitos!");
-     window.location.reload();
-     break;
-    } 
-  }
-  document.getElementById("resultDecod").innerHTML=resultMsgDecod;
-  return resultMsgDecod;
-}
+
